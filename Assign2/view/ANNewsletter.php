da94b21980769bff6d40fac628bff20d3df07159
mexicanpoop <m.a.gregory1@eagle.clarion.edu>
2015-02-19 09:43:08 -0500

    Added new footer and the newsletter page
    yeah

---
+++ Assign2/view/ANNewsletter.php
@@ -0,0 +1,28 @@
+<?php
+	$title = "AudioNexus - Newsletter";
+	include './headerinclude.php';
+?>
+<br>				
+<section>
+<div class="container">
+<div class="jumbotron">
+<h3>Subscribe To our newsletter</h3>
+<p>Subscribe to our newsletter to see the latest news in upcoming musicians, lessons, and
+other exciting news about Audio-Nexus. </p>
+<p><a class="btn btn-success" href="../view/ANUnderconstruction.html" role="button">Subscribe Now</a></p>
+<hr />
+<h3>February 2015</h3>
+<p>Our site is now live! <a href="../view/ANUnderconstruction.html">Sign up today</a> to connect with other musicicans now,
+and feel free to check out our <a href="../view/ANAbout.html">About</a> page to learn more about AudioNexus.</p>
+		
+</div>
+</div>
+</section>
+<br>
+ <?php
+	
+	include './footerinclude.php';
+?>
+
+
+
