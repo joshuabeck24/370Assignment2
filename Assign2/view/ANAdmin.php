efe3a4de555e92ae08912ca7f64408e700fd9b4b
mexicanpoop <m.a.gregory1@eagle.clarion.edu>
2015-02-19 09:53:27 -0500

    Added under construction
    reflected changes in their links

--- Assign2/view/ANAdmin.php
+++ Assign2/view/ANAdmin.php
@@ -8,11 +8,11 @@
  <div class="container">
          <div class="jumbotron">
          <h3 style="text-align:center">ADMIN LOGIN</h3>
-                <form class="formFormat" action="../view/ANUnderconstruction.html">
+                <form class="formFormat" action="../view/ANUnderconstruction.php">
                   Username: <input type="text" name="un"><br><br>
                   Password: <input type="text" name="pw"><br><br>
                 </form>
-        <p style="text-align:center"><a class="btn btn-success" href="../view/ANUnderconstruction.html" role="button">Log In</a></p>
+        <p style="text-align:center"><a class="btn btn-success" href="../view/ANUnderconstruction.php" role="button">Log In</a></p>
         </div>
 </div>
 </section>
---
+++ Assign2/view/ANUnderconstruction.php
@@ -0,0 +1,22 @@
+<?php
+	$title = "AudioNexus - Under Construction";
+	include './headerinclude.php';
+?>
+<img alt="Image of a Mixer" src="../images/mixer.png" class="imageLoad" />
+
+
+<section style="margin-top:-80px;">
+<div class="container">
+<div class="jumbotron">
+<h2>Under Construction</h2>
+<p> This page isn't quite ready yet to be shown just yet.</p>
+</div>
+</div>
+</section>
+<br>
+
+ <?php
+include './footerinclude.php';
+?>
+
+
