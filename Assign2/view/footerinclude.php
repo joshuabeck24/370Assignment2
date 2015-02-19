9f485f4c476ba3bf9ce7485238a1d98ec9293839
Joshua Beck <joshbeck24@gmail.com>
2015-02-19 10:00:23 -0500

    Added Bout to footer include

--- Assign2/view/footerinclude.php
+++ Assign2/view/footerinclude.php
@@ -1,8 +1,9 @@
-</section> <!-- END OF SECTION IN HEADER INCLUDE -->
+<!-- </section> END OF SECTION IN HEADER INCLUDE -->
 <footer id="mainFooter">
 <a class="br" href="mailto:michele.a.gregory@gmail.com">Comments</a>
+<a class="br" href="ANAbout.php">About</a>
 					<a style="text-decoration:none">Copyright &copy; 2015</a>
-		            <a href="ANNewsletter.php" style="text-decoration:none"> <img title="Join Our Newsletter!" class="socialMediaIcons" src="../images/mail.png" width="50" height="50" float="right" /> </a>
+		            <a href="ANNewsletter.html" style="text-decoration:none"> <img title="Join Our Newsletter!" class="socialMediaIcons" src="../images/mail.png" width="50" height="50" float="right" /> </a>
 					<ul class="pull-right" style="list-style-type: none;">
 				         <li class="pull-left"><a href="https://www.facebook.com/"><img class="socialMediaIcons" src="../images/Ifacebook.png" alt="Facebook Icon" title="Go To Facebook" /></a></li>
 				         <li class="pull-left"><a href="https://twitter.com/"><img class="socialMediaIcons" src="../images/Itwitter.png" alt="Twitter Icon" title="Go To Twitter" /></a></li>
