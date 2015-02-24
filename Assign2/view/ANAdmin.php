458cf452cc2bcbbb84250a9e5b7c8fc54c2cea51
Joshua Beck <joshbeck24@gmail.com>
2015-02-23 19:01:44 -0500

    Margin Fix
    Fixed margin on about and admin pages then removeed what offset it in
    bootstrap

--- Assign2/view/ANAdmin.php
+++ Assign2/view/ANAdmin.php
@@ -6,13 +6,13 @@
 		
 <section>
  <div class="container">
-         <div class="jumbotron">
+         <div style="margin-top: 100px;" class="jumbotron">
          <h3 style="text-align:center">ADMIN LOGIN</h3>
-                <form class="formFormat" action="../view/ANUnderconstruction.php">
+                <form class="formFormat" action="../view/ANUnderconstruction.html">
                   Username: <input type="text" name="un"><br><br>
                   Password: <input type="text" name="pw"><br><br>
                 </form>
-        <p style="text-align:center"><a class="btn btn-success" href="../view/ANUnderconstruction.php" role="button">Log In</a></p>
+        <p style="text-align:center"><a class="btn btn-success" href="../view/ANUnderconstruction.html" role="button">Log In</a></p>
         </div>
 </div>
 </section>
