b39b36b951a1733130b8c94f11b56c3254c7fb47
joshuabeck24 <joshbeck24@gmail.com>
2015-03-01 15:13:10 -0500

    FIXED FILE UPLOADS!
    Now the processFileUpload needs to be coded

--- Assign2/view/ANFileupload.php
+++ Assign2/view/ANFileupload.php
@@ -9,11 +9,20 @@
         <div style="margin-top:20px;" class="jumbotron">
             <h2>File Upload</h2>
             <div >
-                <form enctype="multipart/form-data" action="processFileUpload.php" method="post">
+               <form style=" margin: 0;
+                              text-align: center;"
+                      enctype="multipart/form-data" action="processFileUpload.php" method="post">
                     <div style="padding-left:30%;" class=" pull-left divInJumbo">Newsletter(.html)</div><input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
                     <input style="clear:both;" class="btn btn-success" type="submit" value="Send Newsletter" /> <br/>
-                    <hr/>
-                    <div style="padding-left:22%;" class=" pull-left divInJumbo">Home Page Images(.png)</div><input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
+                    
+                    <!-- <div style="padding-left:22%;" class=" pull-left divInJumbo">Home Page Images(.png)</div><input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
+                    <input style="clear:both;" class="btn btn-success" type="submit" value="Send Image" /> <br/>-->
+               </form>
+               <hr/>
+               <form style=" margin: 0;
+                              text-align: center;"
+                      enctype="multipart/form-data" action="processFileUpload.php" method="post">
+                    <div style="padding-left:24.5%;" class=" pull-left divInJumbo">Home Page Images(.png)</div><input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
                     <input style="clear:both;" class="btn btn-success" type="submit" value="Send Image" /> <br/>
                </form>
             </div>
