fa6b079d3fed2b5126ff318d318ddae9d8347802
Joshua Beck <joshbeck24@gmail.com>
2015-03-03 09:48:50 -0500

    Added quote upload

--- Assign2/view/ANFileupload.php
+++ Assign2/view/ANFileupload.php
@@ -13,7 +13,7 @@
                               text-align: center;"
                       enctype="multipart/form-data" action="processFileUpload.php" method="post">
                     <div style="padding-left:30%;" class=" pull-left divInJumbo">Newsletter(.html)</div><input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
-                    <input style="clear:both;" class="btn btn-success" type="submit" value="Send Newsletter" /> <br/>
+                    <input style="clear:both;" class="btn btn-success" type="submit" value="Upload Newsletter" /> <br/>
                     
                     <!-- <div style="padding-left:22%;" class=" pull-left divInJumbo">Home Page Images(.png)</div><input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
                     <input style="clear:both;" class="btn btn-success" type="submit" value="Send Image" /> <br/>-->
@@ -23,7 +23,14 @@
                               text-align: center;"
                       enctype="multipart/form-data" action="processFileUpload.php" method="post">
                     <div style="padding-left:19%;" class=" pull-left divInJumbo">Home Page Images(.jpg, .png, .gif)</div><input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
-                    <input style="clear:both;" class="btn btn-success" type="submit" value="Send Image" /> <br/>
+                    <input style="clear:both;" class="btn btn-success" type="submit" value="Upload Image" /> <br/>
+               </form>
+               <hr/>
+               <form style=" margin: 0;
+                              text-align: center;"
+                      enctype="multipart/form-data" action="processFileUpload.php" method="post">
+                    <div style="padding-left:31%;" class=" pull-left divInJumbo">Quote File(.txt)</div><input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
+                    <input style="clear:both;" class="btn btn-success" type="submit" value="Upload Quote File" /> <br/>
                </form>
             </div>
         </div>
