e036967f5d1f153b333bf8a0ee9bdea75a817c12
mexicanpoop <m.a.gregory1@eagle.clarion.edu>
2015-03-19 10:15:34 -0400

    controller
    made da controller

---
+++ Assign2/controller/controller.php
@@ -0,0 +1,60 @@
+<?php
+  if (isset($_POST['action'])){
+      $action = $_POST['action'];
+  }
+  else if(isset($_GET['action'])){
+      $action = $_GET['action'];
+  }
+  else{
+      include '../view/index.php';
+  }
+      
+  
+  switch($action){
+      case 'About' :
+          include '../view/ANAbout.php';
+          break;
+      case 'Admin':
+          include '../view/ANAdmin.php';
+          break;
+      case 'EmailSend':
+          include '../view/ANEmailsend.php';
+          break;
+      case 'FileUpload':
+          include '../view/ANFileUpload.php';
+          break;
+      case 'Ideas':
+          include '../view/ANIdeas.php';
+          break;
+      case 'Music':
+          include '../view/ANMusic.php';
+          break;
+      case 'Newsletter':
+          include '../view/ANNewsletter.php';
+          break;
+      case 'ProcessRegisterMember':
+          include '../view/ANProcessRegisterMember.php';
+          break;
+      case 'Signin':
+          include '../view/ANSignup.php';
+      case 'UnderConstruction':
+          include '../view/ANUnderConstruction.php';
+          break;
+      case 'Checksheet':
+          include '../view/checksheet.php';
+          break;
+      case 'navinclude':
+          include '../view/navinclude.php';
+          break;
+      case 'ProcessFileUpload':
+          include '../view/processFileUpload.php';
+           break;
+      default:
+          include('../view/index.php');
+  }
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
