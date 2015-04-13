826872baa2679507d7f64c57d76f0cf10413fc49
Joshua Beck <joshbeck24@gmail.com>
2015-04-13 17:02:16 -0400

    This r CRUD

---
+++ Assign2/lib/view/ANSignup.php
@@ -0,0 +1,24 @@
+<?php
+	$title = "AudioNexus - Sign Up";
+	include '../view/headerinclude.php';
+?>
+<br>		
+<section>
+ <div class="container">
+         <div style="margin-top: 100px;" class="jumbotron">
+         <h3 style="text-align:center">Sign in to your account</h3>
+                <form class="formFormat" action="../view/ANUnderconstruction.html">
+                  Username: <input type="text" name="un"><br><br>
+                  Password: <input type="text" name="pw"><br><br>
+                </form>
+          <p style="text-align:center">
+               <a class="btn btn-success" href="../view/ANUnderconstruction.php" role="button">Sign In</a>
+               <a title="Not a Member?"class="btn btn-success" href="../view/ANUnderconstruction.php" role="button">Sign Up</a>
+          </p>
+        </div>
+</div>
+</section>
+<br>
+<?php
+	include '../view/footerinclude.php';
+?>
--- Assign2/view/editMusic.php
+++ Assign2/view/editMusic.php
@@ -9,26 +9,26 @@
 			 <div class="jumbotron container">
 				 <h2>Edit Music</h2>
 				 <hr />
-                    <form id="MusicForm" action ="../controller/controller.php?action=ProcessAddEdit" method="post">
-					  <div class="col-lg-6">
+                    <form enctype="multipart/form-data" class="ma" id="MusicForm" action ="../controller/controller.php?action=ProcessAddEdit" method="post">
+					  <div class="">
 							 <span class="bold">Artist:</span> 
-							 <input type="text" name="Artist" value="<?php echo $row['artistName']; ?>" />
-							 <br />
+							 <input type="text" required=""  maxlength="50" name="Artist" value="<?php echo $ArtistName ?>" />
+							 <br /><br />
 							 <span class="bold">Album:</span>
-							 <input type="text" name="Album" value="<?php echo $row['albumName'] ; ?>" /> 
-	                         <br />
+							 <input type="text" required="" maxlength="50" name="Album" value="<?php echo $AlbumName ?>" /> 
+	                         <br /><br />
 	                         <span class="bold">Song:</span>
-	                         <input type="text" name="Song" value="<?php echo $row['trackName'] ; ?>" />
-	                         <br/>
+	                         <input type="text" required="" maxlength="50" name="Song" value="<?php echo $TrackName ?>" />
+	                         <br/><br />
 	                         <span class="bold">Release Date:</span> 
-	                         <input type="text" name="ReleaseDate" value="<?php echo toDisplayDate($row['releaseDate'])  ?>" />
-	                         <br/>
+	                         <input type="text" required="" placeholder="MM/DD/YYYY" pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d" name="ReleaseDate" value="<?php echo toDisplayDate($ReleaseDate)  ?>" />
+	                         <br/><br />
 	                         <span class="bold">Local:</span> 
-	                         <input type="text" name="IsLocal" value="<?php echo $row['isLocalBand'] ; ?>" />
+	                         <input type="checkbox" name="IsLocal" value="<?php if($IsLocalBand=='Y') echo 'checked' ?>" />
 	                         <!-- Not sure how I want to go about adding the song and its information -->
-							 <input id="input-2" class="rating rating-xs" data-min="0" data-max="5" data-step="0.1" data-size="xs" data-readonly="true" value="<?php echo $row['rating'] ; ?>">
+							 <input name ="Rating" required="" id="input-2" class="rating rating-xs" data-min="0" data-max="5" data-step="0.1" data-size="xs" data-readonly="false" value="<?php echo $Rating ?>">
 							  <div style="padding-left:30%;" class=" pull-left divInJumbo">Song File(.mp3, .m4a, .wav)</div>
-							  <input class="pull-left center-block" name="userfile" type="file" /> <br/><br/>
+							  <input class="pull-left center-block" required="" name="userfile" type="file" /> <br/><br/>
 		                    <input style="clear:both;" class="btn btn-success" type="submit" value="Save" /> <br/>
 					  </div> <!-- Col -->
                   </form>				 
@@ -36,6 +36,15 @@
 		</div> <!-- container -->
 		</section>
 		<br>
+
+		<script>
+			<?php
+			if(!empty($errorLog))
+			{
+				echo "alert('Please Correct the following errors: \\n $errorLog');";
+			}
+			?>
+		</script>
 <?php
     include '../view/footerinclude.php';
 ?>
