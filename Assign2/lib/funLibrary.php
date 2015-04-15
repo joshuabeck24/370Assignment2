825a795a8000020ee7e8ccf59309f9050c44340b
Joshua Beck <joshbeck24@gmail.com>
2015-04-15 16:17:16 -0400

    Merge remote-tracking branch 'origin/Josh' into Josh
    Conflicts:
    	Assign2/controller/controller.php

--- Assign2/lib/funLibrary.php
+++ Assign2/lib/funLibrary.php
@@ -5,10 +5,14 @@
 		if($phpDate = strtotime($date))
 		{return date('m/d/Y',$phpDate);}
 	    else
-	    {
-	    	return "";
-	    }
+	    {return "";}
 	}
-
+    function toMySQLDate($date)
+    {
+    	if($phpDate = strtotime($date))
+    	{return date('Y/m/d',$phpDate);}
+        else
+	    {return "";}
+    }
 
 ?>
